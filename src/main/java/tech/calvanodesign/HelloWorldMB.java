5c27af423649f3c58ae0211da7b8e257dad1426d
Epooch <eric.calvano@gmail.com>
2016-07-13 19:41:58 -0400

    Added test growl message to the welcome page.

--- src/main/java/tech/calvanodesign/HelloWorldMB.java
+++ src/main/java/tech/calvanodesign/HelloWorldMB.java
@@ -1,7 +1,10 @@
 package tech.calvanodesign;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
 import java.io.Serializable;
 
 @ManagedBean
@@ -18,4 +21,21 @@ public class HelloWorldMB implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+    private String message;
+    
+    public String getMessage() {
+        return message;
+    }
+ 
+    public void setMessage(String message) {
+        this.message = message;
+    }
+     
+    public void saveMessage() {
+        FacesContext context = FacesContext.getCurrentInstance();
+         
+        context.addMessage(null, new FacesMessage("Successful",  "Your message: " + message) );
+        context.addMessage(null, new FacesMessage("Second Message", "Additional Message Detail"));
+    }
 }
