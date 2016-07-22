18a976964322ceedc6743096b23db4b8caecbc54
Epooch <eric.calvano@gmail.com>
2016-07-22 01:54:01 -0400

    First attempt at wiring managed bean with the business object.

--- src/main/java/tech/calvanodesign/web/HelloWorldMB.java
+++ src/main/java/tech/calvanodesign/web/HelloWorldMB.java
@@ -1,19 +1,60 @@
 package tech.calvanodesign.web;
 
+import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import tech.calvanodesign.business.HelloWorldBo;
 
 import java.io.Serializable;
 
+/**
+ * 
+ * @author Eric
+ *
+ */
+@Component
 @ManagedBean
 @SessionScoped
 public class HelloWorldMB implements Serializable {
+	
+	@Autowired
+	public HelloWorldBo helloWorldBo;
 
 	private static final long serialVersionUID = 1L;
 	
 	private String name;
+	
+	@PostConstruct
+	public void init () {
+		System.out.println("HelloWorldMB.init()");
+		if (helloWorldBo != null)
+			return;
+		System.out.println("helloWorldBo is null");
+	}
+	
+	public void springTest(ActionEvent e) {
+		// Output some info
+        System.out.println("HelloWorldBean:: Testing Spring imlementation - helloWorldBo.name : " + name);
+        
+     // Call the business object to register the user
+        helloWorldBo.springTest(name);
+        
+        // Set the message here
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Spring test success", "success");  
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+	
+    // Set the registrationBo attribute used by Spring
+    public void setHelloWorldBo(HelloWorldBo helloWorldBo) {
+        this.helloWorldBo = helloWorldBo;
+    }
 
 	public String getName() {
 		return name;
